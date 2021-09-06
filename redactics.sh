d1825ec5bed96a787c0a7936a44dad4cd9290f33
Joe Auty <joe@datasprocket.com>
2021-09-05 21:29:34 -0700

    forget user polling, removal of deprecated backtick notation

--- redactics.sh
+++ redactics.sh
@@ -11,7 +11,7 @@ usage()
   printf '\t\t%s\n' "list-jobs [database ID] (lists all export steps for provided [database ID])"
   printf '\t\t%s\n' "start-job [database ID] (starts new export job for provided [database ID])"
   printf '\t\t%s\n' "start-scan [database ID] (starts new PII scan for provided [database ID])"
-  printf '\t\t%s\n' "forget-user [database ID] [email] (creates user removal SQL queries for provided [database ID] and [email] address)"
+  printf '\t\t%s\n' "forget-user [database ID] [email] (creates user removal SQL queries for provided [database ID] and [email] address and downloads file to local directory)"
   printf '\t\t%s\n' "version (outputs CLI version)"
   printf '\t%s\n' "-h, --help: Prints help"
 }
@@ -39,14 +39,14 @@ EOF
 )
 
 EXPORT_POD_PREFIX=redactics-export-
-NAMESPACE=`helm ls --all-namespaces | grep redactics | awk '{print $2}' | grep redactics`
-VERSION=1.2.0
-KUBECTL=`which kubectl`
-HELM=`which helm`
+NAMESPACE=$(helm ls --all-namespaces | grep redactics | awk '{print $2}' | grep redactics)
+VERSION=1.3.0
+KUBECTL=$(which kubectl)
+HELM=$(which helm)
 
 function check_pod {
   echo "*** WAITING FOR REDACTICS-EXPORT POD TO BE PLACED IN \"$NAMESPACE\" NAMESPACE ***"
-  EXPORT_POD=`kubectl -n $NAMESPACE get pods | grep $EXPORT_POD_PREFIX | grep Running | grep 1/1 | awk '{print $1}'`
+  EXPORT_POD=$(kubectl -n $NAMESPACE get pods | grep $EXPORT_POD_PREFIX | grep Running | grep 1/1 | awk '{print $1}')
 }
 
 function cleanup {
@@ -80,7 +80,7 @@ case "$1" in
 
 list-exports)
   place_export_pod
-  exports=`kubectl -n $NAMESPACE exec $EXPORT_POD -- ls /redacticsdata/export`
+  exports=$(kubectl -n $NAMESPACE exec $EXPORT_POD -- ls /redacticsdata/export)
   printf "\n--- EXPORTS -----------------\n\n$exports\n\n-----------------------------\n"
   cleanup
   ;;
@@ -105,7 +105,7 @@ list-jobs)
     usage
     exit 1
   fi
-  rs=`kubectl -n $NAMESPACE get pods | grep redactics-scheduler | grep Running | grep 1/1 | awk '{print $1}'`
+  rs=$(kubectl -n $NAMESPACE get pods | grep redactics-scheduler | grep Running | grep 1/1 | awk '{print $1}')
   kubectl -n $NAMESPACE -c agent-scheduler exec $rs -- bash -c "/entrypoint.sh airflow list_dag_runs $DAG | grep -A 31 \"id  | run_id\""
   ;;
 
@@ -116,7 +116,7 @@ start-job)
     usage
     exit 1
   fi
-  rs=`kubectl -n $NAMESPACE get pods | grep redactics-scheduler | grep Running | grep 1/1 | awk '{print $1}'`
+  rs=$(kubectl -n $NAMESPACE get pods | grep redactics-scheduler | grep Running | grep 1/1 | awk '{print $1}')
   kubectl -n $NAMESPACE -c agent-scheduler exec $rs -- bash -c "/entrypoint.sh airflow trigger_dag $DATABASE"
   if [ $? == 0 ]
   then
@@ -131,7 +131,7 @@ start-scan)
     usage
     exit 1
   fi
-  rs=`kubectl -n $NAMESPACE get pods | grep redactics-scheduler | grep Running | grep 1/1 | awk '{print $1}'`
+  rs=$(kubectl -n $NAMESPACE get pods | grep redactics-scheduler | grep Running | grep 1/1 | awk '{print $1}')
   kubectl -n $NAMESPACE -c agent-scheduler exec $rs -- bash -c "/entrypoint.sh airflow trigger_dag ${DATABASE}-scanner"
   if [ $? == 0 ]
   then
@@ -147,12 +147,29 @@ forget-user)
     usage
     exit 1
   fi
-  rs=`kubectl -n $NAMESPACE get pods | grep redactics-scheduler | grep Running | grep 1/1 | awk '{print $1}'`
+  rs=$(kubectl -n $NAMESPACE get pods | grep redactics-scheduler | grep Running | grep 1/1 | awk '{print $1}')
   JSON="'{\"email\": \"${EMAIL}\"}'"
   kubectl -n $NAMESPACE -c agent-scheduler exec $rs -- bash -c "/entrypoint.sh airflow trigger_dag -c $JSON ${DATABASE}-usersearch"
   if [ $? == 0 ]
   then
-    printf "YOUR USER REMOVAL REQUEST HAS BEEN QUEUED!\n\nTo track progress, enter \"redactics list-jobs ${DATABASE}-usersearch\". Both the results and any errors will be reported to your Redactics account (https://app.redactics.com/usecases/dataprivacy).\n"
+    poll="running"
+    until [ $poll = "success" ] || [ $poll = "failed" ]
+    do
+      printf "WAITING FOR JOB COMPLETION...\n"
+      last_run=$(kubectl -n $NAMESPACE -c agent-scheduler exec $rs -- bash -c "/entrypoint.sh airflow list_dag_runs ${DATABASE}-usersearch | grep -A 2 \"id  | run_id\" | tail -n 1")
+      poll=$(echo $last_run | awk '{print $5}')
+      sleep 3
+    done
+
+    if [ $poll = "success" ]
+    then
+      DOWNLOAD="forgetuser-queries-${EMAIL}.sql"
+      place_export_pod
+      echo "*** DOWNLOADING $DOWNLOAD ***"
+      kubectl -n $NAMESPACE cp ${EXPORT_POD}:redacticsdata/export/$DOWNLOAD $DOWNLOAD || cleanup
+    else
+      printf "ERROR: there has been a problem with this request. Please check your Redactics account for more information."
+    fi
   fi
   ;;
 
